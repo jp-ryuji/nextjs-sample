6caea20375c9fc386c08feaca09dc26c952cc4bd
Ryuji Eguchi <jp.ryuji@gmail.com>
2023-04-01 16:49:19 +1100

    modify index.tsx to render mocked data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,67 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 
-export default function Home() {
+import { useState } from 'react'
+import { Book, Review } from '../mocks/types'
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Head>
+//         <title>Next Sample</title>
+//         <meta name="description" content="next sample" />
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+//       <main>
+//         <h1>Next Sample</h1>
+//       </main>
+//     </>
+//   )
+// }
+
+type Props = {
+  book: Book
+}
+
+export default function Home({ book }: Props) {
+  const [reviews, setReviews] = useState<Review[] | null>(null)
+
+  const handleGetReviews = () => {
+    // Client-side request are mocked by `mocks/browser.ts`.
+    fetch('/reviews')
+      .then((res) => res.json())
+      .then(setReviews)
+  }
+
   return (
-    <>
-      <Head>
-        <title>Next Sample</title>
-        <meta name="description" content="next sample" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1>Next Sample</h1>
-      </main>
-    </>
+    <div>
+      <img src={book.imageUrl} alt={book.title} width="250" />
+      <h1>{book.title}</h1>
+      <p>{book.description}</p>
+      <button onClick={handleGetReviews}>Load reviews</button>
+      {reviews && (
+        <ul>
+          {reviews.map((review) => (
+            <li key={review.id}>
+              <p>{review.text}</p>
+              <p>{review.author}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
+}
+
+export async function getServerSideProps() {
+  // Server-side requests are mocked by `mocks/server.ts`.
+  const res = await fetch('https://my.backend/book')
+  const book = await res.json()
+
+  return {
+    props: {
+      book,
+    },
+  }
 }
